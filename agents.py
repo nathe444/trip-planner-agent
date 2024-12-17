040b411619005486d2c55b27d71d0f07f943226c
unknown <natnaelm552@gmail.com>
2024-12-17 09:20:57 +0300

    agents complete

--- agents.py
+++ agents.py
@@ -1,30 +1,40 @@
-# from crewai import Agent
-# from textwrap import dedent
-# from langchain_groq import ChatGroq
+from crewai import Agent
+from textwrap import dedent
+from langchain_groq import ChatGroq
 
-# class CustomAgents:
-#     def __init__(self):
-#         self.groq = ChatGroq()
+class CustomAgents:
+    def __init__(self):
+        self.groq = ChatGroq()
         
 
-#     def expert_travel_agent(self):
-#         return Agent(
-#             role="Expert travel agent",
-#             backstory=dedent(f"""I am an expert travel agent.I am expert in travel planning
-#                                  and logistics.I have decades of experience making travel iteneraries"""),
-#             goal=dedent(f"""Create a 7 day itinerary with a detailed plan for each day,
-#                             include budget,packaging suggestions and safety tips"""),
-#             # tools=[tool_1, tool_2],
-#             verbose=True,
-#             llm=self.groq,
-#         )
+    def expert_travel_agent(self):
+        return Agent(
+            role="Expert travel agent",
+            backstory=dedent(f"""I am an expert travel agent.I am expert in travel planning
+                                 and logistics.I have decades of experience making travel iteneraries"""),
+            goal=dedent(f"""Create a 7 day itinerary with a detailed plan for each day,
+                            include budget,packaging suggestions and safety tips"""),
+            # tools=[tool_1, tool_2],
+            verbose=True,
+            llm=self.groq,
+        )
 
-#     def city_selection_expert(self):
-#         return Agent(
-#             role="City selection expert",
-#             backstory=dedent(f"""I am a city search expert.I have decades of experience"""),
-#             goal=dedent(f"""Define agent 2 goal here"""),
-#             # tools=[tool_1, tool_2],
-#             verbose=True,
-#             llm=self.OpenAIGPT35,
-#         )
+    def city_selection_expert(self):
+        return Agent(
+            role="City selection expert",
+            backstory=dedent(f"""I am a city selection expert.I am expert in analyzing travel data and pick ideal destinations"""),
+            goal=dedent(f"""select the best city based on weather,season,prices and travel interests."""),
+            # tools=[tool_1, tool_2],
+            verbose=True,
+            llm=self.groq,
+        )
+
+    def local_tour_guide_expert(self):
+        return Agent(
+            role="Local tour guide expert",
+            goal=dedent(f""" Provide the best insights about the selected city """),
+            backstory = dedent(f"""knowledgable local guide with extensive information about the city, including historical landmarks, popular attractions, and local culture."""),
+            # tools=[tool_1, tool_2],
+            verbose=True,
+            llm=self.groq,
+        )
